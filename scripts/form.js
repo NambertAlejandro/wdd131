315ef4ea3138d15aa340129c074ff7f9b82054e0
NambertAlejandro <neleuterio@byupathway.edu>
2025-12-01 20:14:06 -0300

    updating w5

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,3 @@
-// === ARRAY DE PRODUTOS ===
 const products = [
   { id: 1, name: "Keyboard" },
   { id: 2, name: "TV" },
@@ -6,25 +5,21 @@ const products = [
   { id: 4, name: "Speaker" },
 ];
 
-// === PREENCHER O SELECT ===
 const select = document.getElementById("choose-product");
 
-// Remove as opções fixas que você colocou no HTML
-select.innerHTML = `<option value="" disabled selected>Choose a Product</option>`;
+select.innerHTML = `<option value="" disabled selected>Select a Product...</option>`;
 
 products.forEach((product) => {
   const opt = document.createElement("option");
-  opt.value = product.id; // value = id
-  opt.textContent = product.name; // texto visível
+  opt.value = product.id;
+  opt.textContent = product.name;
   select.appendChild(opt);
 });
 
-// === CONFIGURAR FORM ===
 const form = document.querySelector("form");
 form.method = "get";
 form.action = "review.html";
 
-// === AJUSTAR RADIOS (todos precisam ter o MESMO 'name') ===
 const stars = document.querySelectorAll('input[type="radio"]');
 
 st;
